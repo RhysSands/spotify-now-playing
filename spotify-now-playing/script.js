7b1ac5be66503df451bfdfd14eb49b801cffd7ea
Rhys Sands <sanderr358@hotmail.com>
2025-12-19 18:49:32 -0500

    Delete spotify-now-playing directory

--- spotify-now-playing/script.js
+++
@@ -1,4 +0,0 @@
-// Static demo values
-document.getElementById('track-name').textContent = "Viva La Vida";
-document.getElementById('artist-name').textContent = "Coldplay";
-document.getElementById('album-art').src = "https://i.scdn.co/image/ab67616d0000b2738b173e3b1a85743fba67f8f7";
