4d3ce1eb7d0ceb7ef83b6358c88b3ad9a23a3475
Rhys Sands <sanderr358@hotmail.com>
2025-12-19 19:14:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('track-name').textContent = "iluvoil";
+document.getElementById('artist-name').textContent = "diddy";
+document.getElementById('album-art').src = "https://i.ibb.co/fGq8fLDQ/4e83fc1bf54d2189a9d7f6adf7411d3c.png";
