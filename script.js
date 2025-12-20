3c8e75b37833c0dad98399359b49c8c5c4cab565
Rhys Sands <sanderr358@hotmail.com>
2025-12-19 19:42:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Static demo content
+document.getElementById('track-name').textContent = "The Back Room";
+document.getElementById('artist-name').textContent = "Your Artist";
+document.getElementById('album-art').src = "https://i.ibb.co/fGq8fLDQ/4e83fc1bf54d2189a9d7f6adf7411d3c.png";
