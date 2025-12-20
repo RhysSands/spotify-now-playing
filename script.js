33ff7ab7741c45f1597e3ccc7e800833333e6a3c
Rhys Sands <sanderr358@hotmail.com>
2025-12-19 19:41:48 -0500

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('track-name').textContent = "The Back Room";
-document.getElementById('artist-name').textContent = "Your Artist";
-document.getElementById('album-art').src = "https://i.ibb.co/fGq8fLDQ/4e83fc1bf54d2189a9d7f6adf7411d3c.png";
