2a25b9171258ce4e8f753b2669196d2c63415b8e
Rhys Sands <sanderr358@hotmail.com>
2025-12-19 19:04:32 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Static demo values for now
-document.getElementById('track-name').textContent = "The Back Room";
-document.getElementById('artist-name').textContent = "Your Artist";
-document.getElementById('album-art').src = "https://i.ibb.co/fGq8fLDQ/4e83fc1bf54d2189a9d7f6adf7411d3c.png";
